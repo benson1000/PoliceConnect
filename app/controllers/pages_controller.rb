f875bae59b618edfb91587d80ff9ea6ccda51945
benson1000 <bensonkimani1000@gmail.com>
2023-08-12 13:43:43 +0300

    Webpages added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+    def home
+        #home page
+    end
+    def about
+        #about page
+    end
+    def contact
+        #contact page
+    end
+end
