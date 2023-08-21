6b4752331c45d0836c5cff6ad1b4030a3af15c3a
benson1000 <bensonkimani1000@gmail.com>
2023-08-21 16:19:32 +0300

    Home & about pages added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,12 @@ class PagesController < ApplicationController
     def contact
         #contact page
     end
+
+    def background
+        #about/background page
+    end
+    def vision_mission_core_values
+        #the mission vision
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/about', to: 'pages#about'
+  get '/background', to: 'pages#background'
+  get '/vision_mission_core_values', to: 'pages#vision_mission_core_values'
   get '/contact', to: 'pages#contact'
   root to: 'pages#home'
 end
